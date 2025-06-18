c366285d8e400bc2cf2e5da6c7111aeecc6c5e90
Bryan Macedo <contato.bryansm@hotmail.com>
2025-06-18 15:50:25 -0300

    Estudos sobre estrutura de dados, parte 2 - map, set, list e queue.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,60 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        //map set list queue
+
+        List<String> list = new ArrayList<>();
+        list.add("Bryan");
+        list.add("Macedo");
+        list.add("Bryan");
+        list.add("Soares");
+
+        System.out.println("List: " + list);
+
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("Bryan");
+        stringSet.add("Macedo");
+        stringSet.add("Soares");
+        stringSet.add("Bryan");
+
+        System.out.println("Set: " + stringSet);
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "Bryan");
+        map.put("username", "soma");
+
+        System.out.println(map.get("username"));
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Bryan");
+        queue.add("Simone");
+
+        System.out.println("Queue: " + queue);
+        System.out.println("Queue: " + queue.poll());
+        System.out.println("Queue: " + queue.poll());
+        System.out.println("Queue: " + queue.poll());
+        System.out.println("Queue: " + queue);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
